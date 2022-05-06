70f38c232954cb7062ce06950db5c7a89b9fee5b
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-05-06 15:53:23 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,21 +2,49 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
-}
+class Battery{
+    public:
+        float temperature,soc,chargeRate;
+    bool checkTempIsInRange(float t);
+    bool checkSocIsInRange(float s);
+    bool checkChargeRateIsInRange(float c);
+    bool checkBatteryIsOk(float t,float s ,float c);
+};
 
-int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+class NewBattery:public Battery
+{  
+    public:
+    bool checkTempIsInRange(float temperature)
+    {
+        if(temperature < 0 || temperature > 45)
+            return false;
+        else 
+        return true;
+    }
+    bool checkSocIsInRange(float soc)
+    {
+        if(soc < 20 || soc > 80)
+            return false;
+        else
+            return true;
+        
+    }
+    bool checkChargeRateIsInRange(float chargeRate)
+    {
+        if(chargeRate>0.8)
+            return false;
+        else 
+            return true;
+    }
+    bool checkBatteryIsOk(float t,float s ,float c)
+    {
+        return(checkTempIsInRange(t)&&checkSocIsInRange(s)&&checkChargeRateIsInRange(c));
+    }
+};
+
+
+int main() 
+{
+   NewBattery t;
+   assert(t.checkBatteryIsOk(10,30,0.7)==true);
 }
